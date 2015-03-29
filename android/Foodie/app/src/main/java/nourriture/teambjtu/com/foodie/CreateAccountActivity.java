f62f96775172fe77253b9fc73d4d865e92933025
anteno_y <anteno_y@epitech.eu>
2015-03-29 18:03:25 +0800

    [FEATURE] Create a Register Page for the users : Username, Password, Email, City, Birthday, Gender

--- android/Foodie/app/src/main/java/nourriture/teambjtu/com/foodie/CreateAccountActivity.java
+++ android/Foodie/app/src/main/java/nourriture/teambjtu/com/foodie/CreateAccountActivity.java
@@ -4,14 +4,82 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
 public class CreateAccountActivity extends ActionBarActivity {
+
+    public EditText UsernameEditText;
+    public EditText PasswordEditText;
+    public EditText EmailEditText;
+    public EditText CityEditText;
+    public EditText DateEditText;
+
+    public Spinner GenderSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        UsernameEditText = (EditText) findViewById(R.id.UserNameEditText);
+        PasswordEditText = (EditText) findViewById(R.id.PasswordEditText);
+        EmailEditText = (EditText) findViewById(R.id.EmailEditText);
+        CityEditText = (EditText) findViewById(R.id.CityEditText);
+        DateEditText = (EditText) findViewById(R.id.DateEditText);
+
+        GenderSpinner = (Spinner) findViewById(R.id.GenderChoiceSpinner);
+
+
+        //Clear UsernameEditText OnClick
+        UsernameEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UsernameEditText.setText("");
+            }
+        });
+
+        //Clear PasswordEditText OnClick
+        PasswordEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PasswordEditText.setText("");
+            }
+        });
+
+        //Clear EmailEditText OnClick
+        EmailEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EmailEditText.setText("");
+            }
+        });
+
+        //Clear CityEditText OnClick
+        CityEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CityEditText.setText("");
+            }
+        });
+
+        //Clear DateEditText OnClick
+        DateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DateEditText.setText("");
+            }
+        });
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> GenderAdapter = ArrayAdapter.createFromResource(this, R.array.gender_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        GenderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        GenderSpinner.setAdapter(GenderAdapter);
     }
 
 
